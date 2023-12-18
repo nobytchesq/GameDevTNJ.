c5761bc64c1349a8ce29e855c3636fbc287b3c58
nobytchesq <152158730+nobytchesq@users.noreply.github.com>
2023-12-18 23:44:35 +0700

    Create Shooter.cs

---
+++ Shooter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 20f;
+	public int damage = 40;
+	public Rigidbody2D rb;
+	public GameObject impactEffect;
+
+	// Use this for initialization
+	void Start () {
+		rb.velocity = transform.right * speed;
+	}
+
+	void OnTriggerEnter2D (Collider2D hitInfo)
+	{
+		var enemy = hitInfo.GetComponent<Enemyshu>();
+		if (enemy != null)
+		{
+			enemy.TakeDamage(damage);
+            Destroy(gameObject);
+		}
+
+		Instantiate(impactEffect, transform.position, transform.rotation);
+	}
+	
+}
